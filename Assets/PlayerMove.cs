4b26805fbf1777470123e77315805ceedeb0db5e
LiliaIsABell <lilisa_alugo@yahoo.com>
2021-02-22 16:32:45 -0500

    Initial commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code from Brackeys
+// https://www.youtube.com/watch?v=_QajrabyTJc&t=622s
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController controller;
+    
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        
+    }
+}
